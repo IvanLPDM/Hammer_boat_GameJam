dce6bae641ea5a0c536c18f9a0665a3f3c1ddcdf
MAM333 <miguelangelmontero333@gmail.com>
2026-02-04 19:53:36 +0100

    playerMovementDash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,16 @@ public class Player : MonoBehaviour
     [Header("Turning")]
     public float turnTorque = 5f;
     public float maxAngularSpeed = 1.5f;
+    public float angularDamping = 10f;
+
+    [Header("Dash")]
+    public float dashStatus = 0f;
+    public float dashSpeedMax = 50f;
+    public bool statusUp = true;
+    public float speedStatus = 0.2f;
+    public bool dashing = false;
+    public float dashTime = 0f;
+    public float dashMaxTime = 2f;
 
     Rigidbody rb;
 
@@ -21,9 +31,7 @@ public class Player : MonoBehaviour
         rb.maxAngularVelocity = maxAngularSpeed;
     }
 
-
-
-    void FixedUpdate()
+    private void Movement()
     {
         // Avanzar
         if (Input.GetKey(KeyCode.W))
@@ -36,16 +44,83 @@ public class Player : MonoBehaviour
         {
             rb.AddTorque(Vector3.up * -turnTorque, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             rb.AddTorque(Vector3.up * turnTorque, ForceMode.Force);
         }
-
-        // Limitar velocidad lineal
-        if (rb.velocity.magnitude > maxSpeed)
+        else
         {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            rb.angularVelocity = Vector3.Lerp(
+                rb.angularVelocity,
+                Vector3.zero,
+                angularDamping * Time.fixedDeltaTime
+            );
         }
+
+        // Limitar velocidad angular máxima
+        if (rb.angularVelocity.magnitude > maxAngularSpeed)
+        {
+            rb.angularVelocity = rb.angularVelocity.normalized * maxAngularSpeed;
+        }
+
+        if (!dashing)
+        {
+            // Limitar velocidad lineal
+            //if (rb.velocity.magnitude > maxSpeed)
+            //{
+
+            //    Vector3 excess = rb.velocity.normalized * (rb.velocity.magnitude - maxSpeed);
+            //    rb.AddForce(-excess * 0.1f, ForceMode.VelocityChange);
+            //    //rb.velocity = Vector3.Lerp(rb.velocity, rb.velocity.normalized * maxSpeed, Time.deltaTime * 5f);
+            //}
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetMouseButtonDown(0) && !dashing)
+        {
+            dashing = true;
+            dashTime = dashStatus * dashMaxTime;
+        }
+
+        float time = Time.deltaTime * speedStatus;
+        if (!dashing)
+        {
+            if (statusUp)
+            {
+                dashStatus += time;
+                if (dashStatus >= 1)
+                {
+                    statusUp = false;
+                }
+            }
+            else
+            {
+                dashStatus -= time;
+                if (dashStatus <= 0)
+                {
+                    statusUp = true;
+                }
+            }
+        }
+        else
+        {
+            rb.AddForce(transform.forward * dashSpeedMax * dashStatus, ForceMode.Force);
+            dashTime -= Time.deltaTime;
+            if (dashTime <= 0)
+            {
+                dashTime = 0;
+                dashing = false;
+                dashStatus = 0f;
+                statusUp = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
     }
 
     void Start()
@@ -56,6 +131,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Dash();
+
     }
 }
