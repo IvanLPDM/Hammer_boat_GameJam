6a3a5102ffc3db99ec35927e20c5467c645b1957
MAM333 <miguelangelmontero333@gmail.com>
2026-02-09 10:03:31 +0100

    UI on Play InWebGL

--- Assets/Scripts/ItemsUI.cs
+++ Assets/Scripts/ItemsUI.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ItemsUI : MonoBehaviour
 {
     public TextMeshProUGUI coinsT, fishesT;
-    public GameObject cositas;
+    public GameObject cositas, menu;
+    private bool toggleMenu = false;
 
     public void ActualizeUI(int numFishes, int numCoins)
     {
         fishesT.text = numFishes.ToString() + "/3";
         coinsT.text = numCoins.ToString();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     void Update()
@@ -20,10 +27,18 @@ public class ItemsUI : MonoBehaviour
         {
             cositas.SetActive(false);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.E))
+        {
+            toggleMenu = !toggleMenu;
+            menu.SetActive(toggleMenu);
+        }
     }
 
     void Awake()
     {
         cositas.SetActive(true);
+        toggleMenu = false;
+        menu.SetActive(false);
     }
 }
